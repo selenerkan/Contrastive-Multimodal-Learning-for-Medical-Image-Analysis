f0045e31604ac012f0473eee28f2e1dd399ab143
selen_erkan <erkan@agrilution.com>
2022-11-22 16:41:06 +0100

    update the model

--- src/model.py
+++ src/model.py
@@ -17,25 +17,25 @@ class AdniModel(LightningModule):
     """
 
         self.layer_1 = nn.Conv3d(in_channels=1, out_channels=3, kernel_size=(
-            3, 3, 3), stride=(1, 1, 1))  # input (192x192x160) output (3x158x190x190)
+            3, 3, 3), stride=(1, 1, 1))  # input (256, 256, 170) output (3, 254, 254, 168)
 
         self.pool1 = nn.MaxPool3d(kernel_size=(
-            2, 2, 2), stride=(2, 2, 2))  # input (3x158x190x190) output (3x79x95x95)
+            2, 2, 2), stride=(2, 2, 2))  # input (3, 254, 254, 168) output (3, 127, 127, 84)
 
         self.layer_2 = nn.Conv3d(in_channels=3, out_channels=6, kernel_size=(
-            3, 3, 3), stride=(1, 1, 1))  # input (3x79x95x95) output (6x77x93x93)
+            3, 3, 3), stride=(1, 1, 1))  # input (3, 127, 127, 84) output (6, 125, 125, 82)
 
         self.pool2 = nn.MaxPool3d(kernel_size=(
-            3, 3, 3), stride=(2, 2, 2))  # input (6x77x93x93) output (6x39x47x47)
+            2, 2, 2), stride=(2, 2, 2), ceil_mode=True)  # input (6, 125, 125, 82) output (6, 63, 63, 41)
 
         self.layer_3 = nn.Conv3d(in_channels=6, out_channels=12, kernel_size=(
-            3, 3, 3), stride=(1, 1, 1))  # input (6x39x47x47) output (12x37x45x45)
+            3, 3, 3), stride=(1, 1, 1))  # input (6, 63, 63, 41) output (12, 61, 61, 39)
 
         self.pool3 = nn.MaxPool3d(kernel_size=(
-            3, 3, 3), stride=(2, 2, 2))  # input (12x37x45x45) output (12x19x23x23)
+            2, 2, 2), stride=(2, 2, 2), ceil_mode=True)  # input (12, 61, 61, 39) output (12, 31, 31, 20)
 
         # # the input dimensions are (Number of dimensions * height * width)
-        self.layer_4 = nn.Linear(181656, 1000)
+        self.layer_4 = nn.Linear(12 * 31 * 31 * 20, 1000)  # 230640
 
         self.layer_5 = nn.Linear(1000, 100)
 
