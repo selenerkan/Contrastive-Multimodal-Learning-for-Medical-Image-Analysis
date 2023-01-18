fix image normalization

src/contrastive_loss_dataset.py
@@ -202,7 +202,7 @@ class ContrastiveLearningViewGenerator(object):
 
         # scale images between [0,1]
         min_val = x.min()
-        x = x - min_val / (x.max() - min_val)
+        x = (x - min_val) / (x.max() - min_val)
 
         x = torch.tensor(x)
 

src/multimodal_dataset.py
@@ -51,7 +51,7 @@ class Multimodal_Dataset(Dataset):
         # change to numpy and scale images between [0,1]
         image = np.array(image, dtype=np.float32)
         min_val = image.min()
-        image = image - min_val / (image.max() - min_val)
+        image = (image - min_val) / (image.max() - min_val)
 
         image = torch.tensor(image)
 