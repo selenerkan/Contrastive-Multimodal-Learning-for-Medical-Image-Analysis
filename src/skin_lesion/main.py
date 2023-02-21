582bded6d0186d3f75a63b8d85eeeb19f2dbe60a
selen_erkan <erkan@agrilution.com>
2023-02-21 15:52:25 +0100

    add baseline model for only image model

--- src/skin_lesion/main.py
+++ src/skin_lesion/main.py
@@ -5,12 +5,52 @@ from pytorch_lightning.callbacks import ModelCheckpoint
 import os
 from ham_settings import csv_dir, supervised_config, CHECKPOINT_DIR, SEED
 from models.ham_supervised_model import SupervisedModel
+from models.image_model import BaselineModel
 from models.ham_multi_loss_model import MultiLossModel
 from ham_dataset import HAMDataModule
 from pytorch_lightning.callbacks import LearningRateMonitor
 import torch
 import torch.multiprocessing
 from datetime import datetime
+
+
+def main_baseline(config=None):
+    '''
+    main function to run the baseline model
+    '''
+
+    print('YOU ARE RUNNING BASELINE FOR HAM DATASET')
+    print(config)
+
+    wandb.init(group='HAM_supervised', project="multimodal_training",
+               entity="multimodal_network", config=config)
+    wandb_logger = WandbLogger()
+
+    # get the model
+    model = BaselineModel(learning_rate=wandb.config.learning_rate,
+                          weight_decay=wandb.config.weight_decay)
+    wandb.watch(model, log="all")
+
+    # load the data
+    data = HAMDataModule(
+        csv_dir, age=wandb.config.age, batch_size=wandb.config.batch_size)
+    data.prepare_data()
+    data.set_supervised_multimodal_dataloader()
+    train_dataloader = data.train_dataloader()
+    val_dataloader = data.val_dataloader()
+
+    accelerator = 'cpu'
+    devices = None
+    if torch.cuda.is_available():
+        accelerator = 'gpu'
+        devices = 1
+
+    # Add learning rate scheduler monitoring
+    lr_monitor = LearningRateMonitor(logging_interval='epoch')
+    trainer = Trainer(accelerator=accelerator, devices=devices,
+                      max_epochs=wandb.config.max_epochs, logger=wandb_logger, callbacks=[lr_monitor], log_every_n_steps=10)
+    trainer.fit(model, train_dataloaders=train_dataloader,
+                val_dataloaders=val_dataloader)
 
 
 def main_supervised_multimodal(config=None):
@@ -21,7 +61,7 @@ def main_supervised_multimodal(config=None):
     print('YOU ARE RUNNING SUPERVISED MULTIMODAL FOR HAM DATASET')
     print(config)
 
-    wandb.init(group='HAM_supervised', project="multimodal_training",
+    wandb.init(group='HAM_baseline', project="multimodal_training",
                entity="multimodal_network", config=config)
     wandb_logger = WandbLogger()
 
@@ -130,8 +170,11 @@ if __name__ == '__main__':
     seed_everything(SEED, workers=True)
     torch.multiprocessing.set_sharing_strategy('file_system')
 
+    # run baseline
+    main_baseline(supervised_config)
+
     # run multimodal
     # main_supervised_multimodal(supervised_config)
 
     # run multiloss model (center + cross entropy + triplet)
-    main_multiloss(supervised_config)
+    # main_multiloss(supervised_config)
